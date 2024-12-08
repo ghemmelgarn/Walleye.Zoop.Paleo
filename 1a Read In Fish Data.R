bc88256891edaefe33e35bed57e6b2b90f24f4a9
ghemmelgarn <gracehemmelgarn@gmail.com>
2024-12-07 19:51:59 -0600

    Calculates sufficient effort and pulls lake area and coordinates from fish database

--- 1a Read In Fish Data.R
+++ 1a Read In Fish Data.R
@@ -1,4 +1,6 @@
 #this script downloads fish data and creates a "raw" fish data csv file called FishData.csv
+#determines which surveys have gear and sufficient effort to use the fish data
+#this also pulls out the nhdid, coordinates, and area for all the exact match lakes - at end of script
 
 library(lubridate)
 library(arrow)
@@ -72,13 +74,13 @@ incl.table <- read.csv("Data/Input/Fish Inclusion Table.csv")%>%
   select(-ZoopTows)%>%
   select(-ZoopMonths)%>%
   select(-FishGear)%>%
-  select(-Match)%>%
+  #select(-Chosen.for.analysis.)%>%
   select(-Notes)%>%
   select(-parentdow.zoop.year)
 
-#filter only rows chosen for analysis, get rid of maybe lakes
+#filter only rows with exact fish/zoop year match
 incl.table <-incl.table %>% 
-  filter(Chosen.for.analysis. == "Yes")
+  filter(Match == "Exact")
 
 #rename dow and year columns to match fish database
 #leading zeros not in either data frame so that should be okay
@@ -89,6 +91,10 @@ incl.table <- incl.table %>%
 incl.table <- incl.table %>% 
   mutate(lake_id = as.character(lake_id))%>% 
   mutate(year = as.double(year))
+
+# #save this inclusion table for use with water quality data
+# #this title means it is cleaned in R and contains only exact fish/zoop matches
+# write_csv(incl.table, "Data/Output/Inclusion.Table.Clean.Exact.csv")
 
 #adjust some DOW / lake_id values in inclusion table to make it match fish database
 #fix Belle lake_id: It is 47004900 in the inclusion table but 47004901 in the fish database
@@ -105,7 +111,7 @@ incl.table$lake_id <- replace(incl.table$lake_id, incl.table$lake_id == 4003500,
 #filter out just the data that matches my selected lake/years, represented by the inclusion table
 
 #first choose the surveys that I want, We will go back for the fish later
-#filter out just the gear I want
+#filter out just the gear I want: standard, shallow, and deep gillnets
 #included one special assessment for White Iron lake - I vetted this and ok to use, was a standard assessment combined with other things too I am just using standard assessment part
 #targeted surveys are acceptable for the specific lakes I specify below that I individually investigated
 #other specific lake things to clean up:
@@ -115,27 +121,48 @@ incl.table$lake_id <- replace(incl.table$lake_id, incl.table$lake_id == 4003500,
 #takes the distinct columns from the mn_data
 #glimpse at end shows me if it did what I wanted
 good.surveys <- mn_data %>% 
-  filter(sampling_method_simple == "gill_net" & (survey_type == "Standard Survey" | survey_type == "HISTORICAL"| survey_type == "Population Assessment"| survey_type == "Re-Survey"| survey_type == "Large Lake Survey"| survey_type == "Initial Survey" | (survey_type == "Special Assessment" & lake_id == 69000400) | (survey_type == "Targeted Survey" & lake_id == 3057600) | (survey_type == "Targeted Survey" & lake_id == 11041500) | (survey_type == "Targeted Survey" & lake_id == 13002700)| (survey_type == "Targeted Survey" & lake_id == 18030800) | (survey_type == "Targeted Survey" & lake_id == 38039300) | (survey_type == "Targeted Survey" & lake_id == 47004600 & year = 2020) | (survey_type == "Targeted Survey" & lake_id == 47006800) | (survey_type == "Targeted Survey" & lake_id == 69025400)))  %>% 
+  filter((sampling_method == "gill_net_standard" | sampling_method == "gill_net_stratified_deep" | sampling_method =="gill_net_stratified_shallow" ) & (survey_type == "Standard Survey" | survey_type == "HISTORICAL"| survey_type == "Population Assessment"| survey_type == "Re-Survey"| survey_type == "Large Lake Survey"| survey_type == "Initial Survey" | (survey_type == "Special Assessment" & lake_id == 69000400) | (survey_type == "Special Assessment" & lake_id == 69025400) | (survey_type == "Targeted Survey" & lake_id == 3057600) | (survey_type == "Targeted Survey" & lake_id == 11041500) | (survey_type == "Targeted Survey" & lake_id == 13002700)| (survey_type == "Targeted Survey" & lake_id == 18030800) | (survey_type == "Targeted Survey" & lake_id == 38039300) | (survey_type == "Targeted Survey" & lake_id == 47004600 & year == 2020) | (survey_type == "Targeted Survey" & lake_id == 47006800) | (survey_type == "Targeted Survey" & lake_id == 69025400)))  %>% 
   right_join(incl.table, by = c("lake_id", "year")) %>% 
-  distinct(lake_id, year, total_effort_ident, total_effort_1,  sampling_method_simple, sampling_method, flag, survey_type, lakesize, lakesize_units) %>% 
+  distinct(lake_id, year, total_effort_ident, total_effort_1,  sampling_method_simple, sampling_method, survey_type, flag, lakesize, lakesize_units, nhdhr_id, latitude_lake_centroid, longitude_lake_centroid) %>% 
   collect()
 #collect actually brings data into R
 #end up with more rows than the inclusion table because different gillnet types are separated
+#not collecting flag anymore because it confuses things with the join to fish data later
+#note that this DOES NOT INCLUDE 2023 DATA
 
 
-#use this to troubleshoot why certain lakes are and are not included
-# test <- mn_data %>% 
-#   filter(sampling_method_simple == "gill_net" & lake_id == "16007700" & year == 2017)
+# #use this to troubleshoot why certain lakes are and are not included
+# test <- mn_data %>%
+#   filter(sampling_method_simple == "gill_net" & lake_id == "69025400" & year == 2008)
 # glimpse(test)
-# test %>% 
+# test %>%
 #   distinct(survey_type, year, total_effort_ident) %>%
 #   collect() %>% arrange(survey_type) %>% print(n=nrow(.))
 
 #remove NA rows and flagged rows
 #good.surveys.f <- filter(good.surveys, !is.na(total_effort_1) & is.na(flag))
-#not doing this becayse there are no na values for total effort and I want to pulled out flagged nets but not entire surveys
+#not doing this because there are no na values for total effort and I want to pulled out flagged nets but not entire surveys
 
-#at the end of this, I should feel confident that all of these surveys are good quality and ok to use
+#check that effort is sufficient for lake size, based on DNR sampling manual
+#fix area for upper red, says 0 here but should be 119295 based on lakefinder
+good.surveys$lakesize <- replace(good.surveys$lakesize, good.surveys$lakesize == 0, 119295)
+#fix area for Belle, says 1 here but should be 864 based on lakefinder
+good.surveys$lakesize <- replace(good.surveys$lakesize, good.surveys$lakesize == 1, 864)
+#no other areas are too small to be believable, checked the next 2 smallest and they are accurate
+
+#create lake size bin column
+good.surveys <- good.surveys %>%
+  mutate(size.bin = ifelse(lakesize < 100, "<100", ifelse(lakesize >= 100 & lakesize < 300, "100-300", ifelse(lakesize >= 300 & lakesize < 600, "300-600", ifelse(lakesize >= 600 & lakesize < 1500, "600-1500", ">1500")))))
+#create column for minimum net-night effort based on size bin
+good.surveys <- good.surveys %>%
+  mutate(min.effort = ifelse(size.bin == "<100", 0, ifelse(size.bin == "100-300", 6, ifelse(size.bin == "300-600", 9, ifelse(size.bin == "600-1500", 12, 15)))))
+#create a column that subtracts total effort from minimum required effort
+good.surveys$effort.test <- good.surveys$total_effort_1 - good.surveys$min.effort
+#create a column with the effort conclusion
+good.surveys <- good.surveys %>%
+  mutate(fish.effort.sufficient = ifelse (effort.test >= 0, "yes", "no"))
+
+#at the end of this, I should feel confident that all of these surveys are good quality and ok to use - will filter for gear issues later
 
 #GO BACK FOR THE FISH
 fish <- mn_data %>% 
@@ -143,6 +170,8 @@ fish <- mn_data %>%
   collect()
 
 #need to filter out nets flagged with gear issues
+#isolate the flagged fish
+flagged.fish <- filter(fish, flag == "gear issue")
 #then need to adjust total effort for nets removed from analysis
 
 #remove any rows from the fish data flagged with a gear issue - this removes bad sub-efforts (individual nets) but keeps the rest of the survey
@@ -153,13 +182,24 @@ fish <- mn_data %>%
 write_csv(fish, "Data/Output/FishData.csv")
 
 
-#run this instread of line 103 to just get the lat, long, and nhdid on the lakes I want to include
-nhdid.lat.long <- mn_data %>%
-  right_join(incl.table, by = c("lake_id", "year")) %>% 
-  distinct(nhdhr_id, latitude_lake_centroid, longitude_lake_centroid, lake_id, year) %>% 
+
+
+
+
+#PULL JUST THE NHDID, COORDINATES, AND AREA DATA I WANT FOR MY LAKES, YEAR DOESN'T MATTER FOR THIS
+nhdid.lat.long.area <- mn_data %>%
+  right_join(incl.table, by = c("lake_id")) %>% 
+  distinct(lake_id, nhdhr_id, latitude_lake_centroid, longitude_lake_centroid, lakesize, lakesize_units) %>% 
   collect()
+#fix area for upper red, says 0 here but should be 119295 based on lakefinder
+nhdid.lat.long.area$lakesize <- replace(nhdid.lat.long.area$lakesize, nhdid.lat.long.area$lakesize == 0, 119295)
+#fix area for Belle, says 1 here but should be 864 based on lakefinder
+nhdid.lat.long.area$lakesize <- replace(nhdid.lat.long.area$lakesize, nhdid.lat.long.area$lakesize == 1, 864)
+#join back to inclusion table to match this data with all the lakes and years
+lakes.lat.long.area <- left_join(incl.table, nhdid.lat.long.area, by = "lake_id")
 
-
+#write this as a .csv so I can use it to get temp data and join it to the preliminary data
+write_csv(lakes.lat.long.area, "Data/Output/Selected_Lakes_Location_Area.csv")
 
 
 
