37f00414dc1038481b86171db59a0722ff3f3f57
ghemmelgarn <gracehemmelgarn@gmail.com>
2024-12-07 19:52:38 -0600

    Changes order of columns in fish.metric data frame

--- Fish.Metric.Calc.R
+++ Fish.Metric.Calc.R
@@ -1,4 +1,5 @@
-#this script takes the downloaded fish data and calculates walleye CPUE
+#this script takes the downloaded fish data and calculates fish metrics
+#for now just doing walleye CPUE, later on will try to get to biomass with all species
 
 #read in fish data
 
@@ -9,6 +10,6 @@ fish.metric <- fish %>%
   group_by(total_effort_ident) %>% 
   mutate(walleye_count = sum(species_1 == "walleye"), WAE.CPUE = walleye_count/total_effort_1) %>% 
   distinct(total_effort_ident, .keep_all = TRUE) %>% 
-  select(lake_name, lake_id, walleye_count, WAE.CPUE, year.x, total_effort_ident)
+  select(lake_name, lake_id, year.x, total_effort_ident, walleye_count, WAE.CPUE)
 #the distinct part of this collapses each sampling effort into one row but keeps all the columns - will only keep first row with each ident  
 #select chooses the columns we want to keep  
