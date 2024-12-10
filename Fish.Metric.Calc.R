ed7a9072847a28fa7bf5e4eec68431ec962fa707
Grace L Hemmelgarn <hemme129@umn.edu>
2024-12-09 23:21:44 -0600

    Calculates fish metrics for surveys with effort that is not ideal but not terrible

--- Fish.Metric.Calc.R
+++ Fish.Metric.Calc.R
@@ -8,15 +8,29 @@ fish.data <- read.csv("Data/Input/FishData.csv")
 
 #we want cpue of walleye for each lake and year - not grouping by total_effort_ident because I want to sum both catch and effort of shallow and deep nets in stratified surveys
 #using total_effort_cse (combine stratified effort) that I calcualted when I pulled the fish data
-walleye.cpue <- fish %>% 
+walleye.cpue <- fish.data %>% 
   group_by(lake_id, year.x) %>% 
   mutate(walleye_count = sum(species_1 == "walleye"), 
          WAE.CPUE = walleye_count/total_effort_cse
          ) %>% 
   distinct(lake_id, year.x, .keep_all = TRUE) %>% 
-  select(lake_name, lake_id, year.x, walleye_count, WAE.CPUE)
+  select(lake_name, lake_id, year.x, walleye_count, WAE.CPUE, fish.effort.sufficient)
 #the distinct part of this collapses each sampling effort into one row but keeps all the columns - will only keep first row with each ident  
 #select chooses the columns we want to keep  
 
-#save this as a .csv file
-write.csv(walleye.cpue, file = "Data/Output/Walleye_CPUE.csv")
+# #save this as a .csv file
+# write.csv(walleye.cpue, file = "Data/Output/Walleye_CPUE.csv")
+
+
+#SAME THING AS ABOVE BUT THE MORE INCLUSIVE DATA FOR SAMPLE SIZE IMPROVEMENT:
+fish.data.inclusive <- read.csv("Data/Input/FishData.inclusive.csv")
+walleye.cpue.inclusive <- fish.data.inclusive %>% 
+  group_by(lake_id, year.x) %>% 
+  mutate(walleye_count = sum(species_1 == "walleye"), 
+         WAE.CPUE = walleye_count/total_effort_cse
+  ) %>% 
+  distinct(lake_id, year.x, .keep_all = TRUE) %>% 
+  select(lake_name, lake_id, year.x, walleye_count, WAE.CPUE, fish.effort.sufficient)
+
+# #save this as a .csv file
+# write.csv(walleye.cpue.inclusive, file = "Data/Output/Walleye_CPUE.inclusive.csv")
