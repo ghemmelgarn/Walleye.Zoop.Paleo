30718e40d33fc6c6deef35da62ca841cbd5d4815
Grace L Hemmelgarn <hemme129@umn.edu>
2024-12-10 14:07:09 -0600

    Adds largemouth bass count and CPUE to extracted data

--- Fish.Metric.Calc.R
+++ Fish.Metric.Calc.R
@@ -7,14 +7,17 @@ fish.data <- read.csv("Data/Input/FishData.csv")
 #if there is a chance that some of the surveys caught no fish at all, go ask Denver what to do
 
 #we want cpue of walleye for each lake and year - not grouping by total_effort_ident because I want to sum both catch and effort of shallow and deep nets in stratified surveys
-#using total_effort_cse (combine stratified effort) that I calcualted when I pulled the fish data
+#using total_effort_cse (combine stratified effort) that I calculated when I pulled the fish data
+#also want largemouth bass CPUE for a walleye vs. centrarchid plot
 walleye.cpue <- fish.data %>% 
   group_by(lake_id, year.x) %>% 
   mutate(walleye_count = sum(species_1 == "walleye"), 
-         WAE.CPUE = walleye_count/total_effort_cse
+         LMB_count = sum(species_1 == "largemouth_bass"),
+         WAE.CPUE = walleye_count/total_effort_cse,
+         LMB.CPUE = LMB_count/total_effort_cse
          ) %>% 
   distinct(lake_id, year.x, .keep_all = TRUE) %>% 
-  select(lake_name, lake_id, year.x, walleye_count, WAE.CPUE, fish.effort.sufficient)
+  select(lake_name, lake_id, year.x, walleye_count, WAE.CPUE, LMB_count, LMB.CPUE, fish.effort.sufficient)
 #the distinct part of this collapses each sampling effort into one row but keeps all the columns - will only keep first row with each ident  
 #select chooses the columns we want to keep  
 
@@ -27,10 +30,12 @@ fish.data.inclusive <- read.csv("Data/Input/FishData.inclusive.csv")
 walleye.cpue.inclusive <- fish.data.inclusive %>% 
   group_by(lake_id, year.x) %>% 
   mutate(walleye_count = sum(species_1 == "walleye"), 
-         WAE.CPUE = walleye_count/total_effort_cse
+         LMB_count = sum(species_1 == "largemouth_bass"),
+         WAE.CPUE = walleye_count/total_effort_cse,
+         LMB.CPUE = LMB_count/total_effort_cse
   ) %>% 
   distinct(lake_id, year.x, .keep_all = TRUE) %>% 
-  select(lake_name, lake_id, year.x, walleye_count, WAE.CPUE, fish.effort.sufficient)
+  select(lake_name, lake_id, year.x, walleye_count, WAE.CPUE, LMB_count, LMB.CPUE, fish.effort.sufficient)
 
 # #save this as a .csv file
 # write.csv(walleye.cpue.inclusive, file = "Data/Output/Walleye_CPUE.inclusive.csv")
