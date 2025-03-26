1b1cf2f34f2f0558c0ab27dd4b353e8e406bcb05
Grace L Hemmelgarn <hemme129@umn.edu>
2025-03-26 16:49:10 -0500

    Investigates which fish have length and weight data

--- Fish.Metric.Calc.R
+++ Fish.Metric.Calc.R
@@ -1,8 +1,9 @@
-#this script takes the downloaded fish data and calculates fish metrics
-#for now just doing walleye CPUE, later on will try to get to biomass with all species
+#this script takes the downloaded fish data and calculates fish metrics: abundance and CPUE for all species
+#below it investigates which fish we have length/weight data for
 
 library(tidyverse)
 library(dplyr)
+library(ggplot2) #to make exploratory plots below
 
 
 #read in fish data
@@ -350,4 +351,244 @@ all.fish.cpue <- fish.data %>%
 # write.csv(all.fish.cpue, file = "Data/Output/All_Fish_CPUE.csv")
 
 
+#-----------------------------------------------------------------------------------------------------
+#THIS SECTION INVESTIGATES WHICH FISHES HAVE LENGTH/WEIGHT DATA
 
+#read in fish data again if you didn't run the above section
+fish.data <- read.csv("Data/Input/FishData.csv")
+
+# #check that lengths and weights are all the same units 
+# unique(fish.data$length_unit_1)
+# unique(fish.data$weight_unit_1)
+# #looks good
+
+#histogram of all fish lengths that we do have
+hist(fish.data$length_1)
+
+#first create a yes/no column if they have length
+fish.data <- fish.data %>% 
+  mutate(length_yn = ifelse(is.na(length_1), "no", "yes")) %>% 
+  mutate(weight_yn = ifelse(is.na(weight_1), "no", "yes"))
+
+
+
+#count of fish with and without lengths and weights
+table(fish.data$length_yn)
+table(fish.data$weight_yn)
+#we see that most of the fish actually do have lengths
+
+#counts together
+table(fish.data$length_yn, fish.data$weight_yn)
+#all the fish with weights have a length, but not all the fish with lengths have a weight
+
+#LENGTH x SPP
+#lets look at this by species
+table(fish.data$species_1, fish.data$length_yn)
+#make this into a data frame
+length.spp <- data.frame(table(fish.data$species_1, fish.data$length_yn))
+length.spp <- pivot_wider(data = length.spp, names_from = Var2, values_from = Freq)
+length.spp <- rename(length.spp, species = Var1)
+
+#calculate proportion of individuals with lengths for each species
+length.spp$total <- length.spp$no + length.spp$yes
+length.spp$prop <- length.spp$yes / length.spp$total
+
+#plot it
+
+#order species by decreasing proportion to make graph easy to read
+length.spp$species <- factor(length.spp$species, levels = length.spp$species[order(length.spp$prop)])
+#plot
+length.spp.prop <- ggplot(length.spp, aes(x = species, y = prop)) +
+  geom_bar(stat = "identity") +
+  coord_flip() +
+  labs(y = "Proprotion of Individuals with Length Data", x = "Species", title = "Length x Species")+
+  geom_text(aes(label = total), hjust = -0.1)+
+  geom_text(aes(label = "n", x = 68, y = 1.01), vjust = 0.9, size = 5)+
+  theme_classic()
+length.spp.prop
+
+#WEIGHT x SPP
+#lets look at this by species
+table(fish.data$species_1, fish.data$weight_yn)
+#make this into a data frame
+weight.spp <- data.frame(table(fish.data$species_1, fish.data$weight_yn))
+weight.spp <- pivot_wider(data = weight.spp, names_from = Var2, values_from = Freq)
+weight.spp <- rename(weight.spp, species = Var1)
+
+#calculate proportion of individuals with lengths for each species
+weight.spp$total <- weight.spp$no + weight.spp$yes
+weight.spp$prop <- weight.spp$yes / weight.spp$total
+
+#plot it
+
+#order species by decreasing proportion to make graph easy to read
+weight.spp$species <- factor(weight.spp$species, levels = weight.spp$species[order(weight.spp$prop)])
+#plot
+weight.spp.prop <- ggplot(weight.spp, aes(x = species, y = prop)) +
+  geom_bar(stat = "identity") +
+  coord_flip() +
+  labs(y = "Proprotion of Individuals with Weight Data", x = "Species", title = "Weight x Species")+
+  geom_text(aes(label = total), hjust = -0.1)+
+  geom_text(aes(label = "n", x = 68, y = 1.01), vjust = 0.9, size = 5)+
+  theme_classic()
+weight.spp.prop
+
+#LENGTH AND WEIGHT x SPECIES
+#combine the data frames
+length.weight.spp <- rename(length.spp, length.prop = prop)
+length.weight.spp$weight.prop <- weight.spp$prop
+
+#plot them together
+#tiff("Length_Weight_Data_by_species.tiff", width = 15, height = 10, units = "in", res = 300)                   
+length.weight.spp.prop <- ggplot()+
+  geom_bar(data = length.weight.spp, aes(x = species, y = length.prop), stat = "identity", fill = "gray") +
+  geom_bar(data = length.weight.spp, aes(x = species, y = weight.prop), stat = "identity", fill = "lightblue")+
+  coord_flip() +
+  labs(y = "Proprotion of Individuals with Data", x = "Species", title = "Length and Weight x Species")+
+  geom_text(data = length.weight.spp, aes(label = total, x = species, y = length.prop), hjust = -0.1)+
+  geom_text(aes(label = "n", x = 68, y = 1.01), vjust = 0.9, size = 5)+
+  theme_classic()+
+  geom_tile(data = data.frame(x = 4, y = 0.9), aes(x = x, y = y), 
+                            width = 1, height = 0.01, fill = "lightblue") +  # Color box
+  annotate("text", x = 4, y = 0.95, label = "Weight Data", size = 4)+
+  geom_tile(data = data.frame(x = 2, y = 0.9), aes(x = x, y = y), 
+            width = 1, height = 0.01, fill = "gray") +  # Color box
+  annotate("text", x = 2, y = 0.95, label = "Length Data", size = 4)
+  #scale_fill_manual(values = c("length" = "gray", "weight" = "lightblue"))
+length.weight.spp.prop
+#dev.off()
+
+
+#write .csv of length and weight data by species
+lw.export <- rename(length.weight.spp, length.yes = yes, length.no = no)
+lw.export$weight.yes <- weight.spp$yes
+lw.export$weight.no <- weight.spp$no
+#reorder
+lw.export <- lw.export[, c(1,4,3,2,5,7,8,6)]
+#write csv
+#write.csv(lw.export, file = "Data/Output/Fish_Species_Length_Weight_Data.csv")
+
+#LENGTH AND WEIGHT BY LAKE
+table(fish.data$lake_name, fish.data$length_yn)
+#make this into a data frame
+length.lake <- data.frame(table(fish.data$lake_name, fish.data$length_yn))
+length.lake <- pivot_wider(data = length.lake, names_from = Var2, values_from = Freq)
+length.lake <- rename(length.lake, lake = Var1)
+
+#calculate proportion of individuals with lengths for each lake
+length.lake$total <- length.lake$no + length.lake$yes
+length.lake$prop <- length.lake$yes / length.lake$total
+
+#add weight data to data frame too
+lw.lake <- rename(length.lake, length.yes = yes, length.no = no, length.prop = prop)
+#reorder
+lw.lake <- lw.lake[, c(1,4,3,2,5)]
+#create weight data
+table(fish.data$lake_name, fish.data$weight_yn)
+#make this into a data frame
+weight.lake <- data.frame(table(fish.data$lake_name, fish.data$weight_yn))
+weight.lake <- pivot_wider(data = weight.lake, names_from = Var2, values_from = Freq)
+weight.lake <- rename(weight.lake, species = Var1)
+weight.lake$total <- weight.lake$no + weight.lake$yes
+weight.lake$prop <- weight.lake$yes / weight.lake$total
+#add weight data to length data
+lw.lake$weight.yes <- weight.lake$yes
+lw.lake$weight.no <- weight.lake$no
+lw.lake$weight.prop <- weight.lake$prop
+
+#write csv
+#write.csv(lw.lake, file = "Data/Output/Fish_Lake_Length_Weight_Data.csv")
+
+#order lakes by decreasing proportion to make graph easy to read
+lw.lake$lake <- factor(lw.lake$lake, levels = lw.lake$lake[order(lw.lake$length.prop)])
+
+#plot it
+#tiff("Length_Weight_Data_by_Lake.tiff", width = 15, height = 10, units = "in", res = 300)                   
+length.weight.lake.prop <- ggplot()+
+  geom_bar(data = lw.lake, aes(x = lake, y = length.prop), stat = "identity", fill = "gray") +
+  geom_bar(data = lw.lake, aes(x = lake, y = weight.prop), stat = "identity", fill = "lightblue")+
+  coord_flip() +
+  labs(y = "Proprotion of Individuals with Data", x = "Lake", title = "Length and Weight x Lake")+
+  geom_text(data = lw.lake, aes(label = total, x = lake, y = length.prop), hjust = -0.1)+
+  geom_text(aes(label = "n", x = 68, y = 1.01), vjust = 0.9, size = 5)+
+  theme_classic()+
+  geom_tile(data = data.frame(x = 4, y = 0.9), aes(x = x, y = y), 
+            width = 1, height = 0.01, fill = "lightblue") +  # Color box
+  annotate("text", x = 4, y = 0.95, label = "Weight Data", size = 4)+
+  geom_tile(data = data.frame(x = 2, y = 0.9), aes(x = x, y = y), 
+            width = 1, height = 0.01, fill = "gray") +  # Color box
+  annotate("text", x = 2, y = 0.95, label = "Length Data", size = 4)
+#scale_fill_manual(values = c("length" = "gray", "weight" = "lightblue"))
+length.weight.lake.prop
+#dev.off()
+
+#LENGTH AND WEIGHT BY lake/year
+#need to make parentdow fish year columns
+fish_parentdow <- fish.data %>%
+  mutate(parentdow = case_when(
+    nchar(fish.data$lake_id) == 7 ~ substr(lake_id, 1, 5),
+    nchar(fish.data$lake_id) == 8 ~ substr(lake_id, 1, 6)
+  ))
+fish_parentdow$parentdow.fish.year = paste(fish_parentdow$parentdow, fish_parentdow$year.x)
+#look at the table
+table(fish_parentdow$parentdow.fish.year, fish.data$length_yn)
+#make this into a data frame
+length.ly <- data.frame(table(fish_parentdow$parentdow.fish.year, fish.data$length_yn))
+length.ly <- pivot_wider(data = length.ly, names_from = Var2, values_from = Freq)
+length.ly <- rename(length.ly, parentdow.fish.year = Var1)
+
+#calculate proportion of individuals with lengths for each lake
+length.ly$total <- length.ly$no + length.ly$yes
+length.ly$prop <- length.ly$yes / length.ly$total
+
+#add weight data to data frame too
+lw.ly<- rename(length.ly, length.yes = yes, length.no = no, length.prop = prop)
+#reorder
+lw.ly <- lw.ly[, c(1,4,3,2,5)]
+#create weight data
+table(fish_parentdow$parentdow.fish.year, fish.data$weight_yn)
+#make this into a data frame
+weight.ly <- data.frame(table(fish_parentdow$parentdow.fish.year, fish.data$weight_yn))
+weight.ly <- pivot_wider(data = weight.ly, names_from = Var2, values_from = Freq)
+weight.ly <- rename(weight.ly, species = Var1)
+weight.ly$total <- weight.ly$no + weight.ly$yes
+weight.ly$prop <- weight.ly$yes / weight.ly$total
+#add weight data to length data
+lw.ly$weight.yes <- weight.ly$yes
+lw.ly$weight.no <- weight.ly$no
+lw.ly$weight.prop <- weight.ly$prop
+
+#add lake name back in
+name.join <- fish_parentdow%>%
+  group_by(parentdow.fish.year) %>%
+  summarize(lake_name = first(lake_name), year = first(year.x), .groups = 'drop')
+lw.ly.name <- left_join(lw.ly, name.join, by = "parentdow.fish.year")
+#create lake name year column
+lw.ly.name$lake.year = paste(lw.ly.name$lake_name, lw.ly.name$year)
+
+#write csv
+#write.csv(lw.ly.name, file = "Data/Output/Fish_Survey_Length_Weight_Data.csv")
+
+#order lakes by decreasing proportion to make graph easy to read
+lw.ly.name$lake.year <- factor(lw.ly.name$lake.year, levels = lw.ly.name$lake.year[order(lw.ly.name$length.prop)])
+
+#plot it
+#THIS PLOT LOOKS TERRIBLE IN R - BUT IF YOU MAKE THE TIFF WITH HEIGHT = 50 in, ZOOM, AND SCROLL YOU CAN SEE WHAT YOU NEED TO SEE
+#tiff("Length_Weight_Data_by_Survey.tiff", width = 15, height = 50, units = "in", res = 300)                   
+length.weight.surv.prop <- ggplot()+
+  geom_bar(data = lw.ly.name, aes(x = lake.year, y = length.prop), stat = "identity", fill = "gray") +
+  geom_bar(data = lw.ly.name, aes(x = lake.year, y = weight.prop), stat = "identity", fill = "lightblue")+
+  coord_flip() +
+  labs(y = "Proprotion of Individuals with Data", x = "Fish Survey", title = "Length and Weight x Fish Survey")+
+  geom_text(data = lw.ly.name, aes(label = total, x = lake.year, y = length.prop), hjust = -0.1)+
+  geom_text(aes(label = "n", x = 68, y = 1.01), vjust = 0.9, size = 5)+
+  theme_classic()+
+  geom_tile(data = data.frame(x = 4, y = 0.9), aes(x = x, y = y), 
+            width = 1, height = 0.01, fill = "lightblue") +  # Color box
+  annotate("text", x = 4, y = 0.95, label = "Weight Data", size = 4)+
+  geom_tile(data = data.frame(x = 2, y = 0.9), aes(x = x, y = y), 
+            width = 1, height = 0.01, fill = "gray") +  # Color box
+  annotate("text", x = 2, y = 0.95, label = "Length Data", size = 4)
+#scale_fill_manual(values = c("length" = "gray", "weight" = "lightblue"))
+length.weight.surv.prop
+#dev.off()
