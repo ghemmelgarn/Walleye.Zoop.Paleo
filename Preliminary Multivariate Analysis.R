3093e63475e63e110d128e5813bae716ddc620ff
Grace L Hemmelgarn <hemme129@umn.edu>
2025-04-11 14:27:28 -0500

    Creates some notes to consider as I begin multivariate analyses

---
+++ Preliminary Multivariate Analysis.R
@@ -0,0 +1,9 @@
+#to d0: check for and remove uncommon species (too many zeroes) - or apply transformations to correct for this
+#consider standardizing
+#look for any obvious non-linear relationships - transform if needed
+
+#useful website:
+#https://sites.google.com/site/mb3gustame/indirect-gradient-analysis/principal-components-analysis
+
+#another useful website
+#https://r.qcbs.ca/workshop09/book-en/transformations.html
