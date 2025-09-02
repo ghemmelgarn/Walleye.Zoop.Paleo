eadf4e8802a013c9da638f607f33266bb5355630
Grace L Hemmelgarn <hemme129@umn.edu>
2025-09-02 16:30:05 -0500

    Saves figures as .tiff files

--- Sediment Zoop ID Consistency.R
+++ Sediment Zoop ID Consistency.R
@@ -12,9 +12,12 @@ library(tidyr)
 #read in data and make N/A, NA, or an empty cell all read in as NA
 Data <- read.csv("Data/Input/QAQC_Exercise_Sediment_Zoop_ID.csv", na.strings = c("N/A", "NA", ""))
 
+#Set lake name for file names:
+LakeName <- "Green"
 
 #histograms of remain type, remain condition, and taxa by lab worker
 
+#tiff(paste0(LakeName, "_Test_Remain_Type.tiff"), width = 8, height = 6, units = "in", res = 300)
 Remain_Type <- ggplot(Data, aes(x = Remain.Type, fill = IDed.by)) +
   geom_bar(position = "dodge") +
   labs(title = "Remain Type", y = "Count", x = "Remain Type") +
@@ -27,8 +30,10 @@ Remain_Type <- ggplot(Data, aes(x = Remain.Type, fill = IDed.by)) +
   geom_text(stat = "count", aes(label = ..count..), position = position_dodge(width = 0.9), vjust = -0.5)+ # label bars with counts
   scale_y_continuous(limits = c(0, 35))
 Remain_Type  
+#dev.off()
 
 
+#tiff(paste0(LakeName, "_Test_Remain_Condition.tiff"), width = 8, height = 5.5, units = "in", res = 300)
 Remain_Condition <- ggplot(Data, aes(x = Remain.Condition, fill = IDed.by)) +
   geom_bar(position = "dodge") +
   labs(title = "Remain Condition", y = "Count", x = "Remain Condition") +
@@ -41,8 +46,9 @@ Remain_Condition <- ggplot(Data, aes(x = Remain.Condition, fill = IDed.by)) +
   geom_text(stat = "count", aes(label = ..count..), position = position_dodge(width = 0.9), vjust = -0.5)+ # label bars with counts
   scale_y_continuous(limits = c(0, 40))
 Remain_Condition 
+#dev.off()
 
-
+#tiff(paste0(LakeName, "_Test_Taxa.tiff"), width = 11, height = 7, units = "in", res = 300)
 Taxa <- ggplot(Data, aes(x = Taxa, fill = IDed.by)) +
   geom_bar(position = "dodge") +
   labs(title = "Taxa", y = "Count", x = "Taxa") +
@@ -55,10 +61,12 @@ Taxa <- ggplot(Data, aes(x = Taxa, fill = IDed.by)) +
   geom_text(stat = "count", aes(label = ..count..), position = position_dodge(width = 0.9), vjust = -0.5)+ # label bars with counts
   scale_y_continuous(limits = c(0, 20))
 Taxa
+#dev.off()
 
 
 
 Data$Specimen = paste(Data$Taxa, Data$Remain.Type)
+#tiff(paste0(LakeName, "_Test_Taxa_and_Remain_Type.tiff"), width = 14, height = 10, units = "in", res = 300)
 Taxa_and_Remain_Type <- ggplot(Data, aes(x = Specimen, fill = IDed.by)) +
   geom_bar(position = "dodge") +
   labs(title = "Taxa and Remain Type", y = "Count", x = "Taxa and Remain Type") +
@@ -70,7 +78,8 @@ Taxa_and_Remain_Type <- ggplot(Data, aes(x = Specimen, fill = IDed.by)) +
   ) +
   geom_text(stat = "count", aes(label = ..count..), position = position_dodge(width = 0.9), vjust = -0.5)+
   scale_y_continuous(limits = c(0, 20))
-Taxa_and_Remain_Type  
+Taxa_and_Remain_Type
+#dev.off()
 
 #-------------------------------------------------------------------------------------------------------
 #PLOT OUR LENGTHS OF THE THINGS WE MEASURED TO COMPARE THOSE
@@ -96,6 +105,7 @@ Carapaces <- Data %>%
 
 
 #now plot them
+#tiff(paste0(LakeName, "_Test_Antennule_Length.tiff"), width = 10, height = 4.5, units = "in", res = 300)
 Antennule_Length <- ggplot(Bosminid_Headshields, aes(x = Attenule.Length..µm. , fill = IDed.by)) +
   geom_histogram(position = "dodge", binwidth = 10) +
   labs(title = "Antennule Length", y = "Count", x = "Antennule Length (µm)") +
@@ -110,7 +120,9 @@ Antennule_Length <- ggplot(Bosminid_Headshields, aes(x = Attenule.Length..µm. ,
   geom_text(stat = "bin", binwidth = 10, aes(label = ifelse(..count.. > 0, round((..xmax.. + ..xmin..) / 2, 0), ""), y = ..count..), position = position_dodge(width = 0.5), vjust = -0, angle = 60)+ #label bars with x values (bin midpoint)
   scale_y_continuous(limits = c(0, 2.1))
 Antennule_Length
+#dev.off()
 
+#tiff(paste0(LakeName, "_Test_Antennule_Segments.tiff"), width = 10, height = 4.5, units = "in", res = 300)
 Antennule_Segments <- ggplot(Bosminid_Headshields, aes(x = Antennule.Segments , fill = IDed.by)) +
   geom_histogram(position = "dodge", binwidth = 1) +
   labs(title = "Antennule Segments", y = "Count", x = "Antennule Segments (count)") +
@@ -125,7 +137,9 @@ Antennule_Segments <- ggplot(Bosminid_Headshields, aes(x = Antennule.Segments , 
   geom_text(stat = "bin", binwidth = 1, aes(label = ifelse(..count.. > 0, round((..xmax.. + ..xmin..) / 2, 0), ""), y = ..count..), position = position_dodge(width = 0.9), vjust = -0.5)+ #label bars with x values (bin midpoint)
   scale_y_continuous(limits = c(0, 2.1))
 Antennule_Segments
+#dev.off()
 
+#tiff(paste0(LakeName, "_Test_Mucro_Length.tiff"), width = 10, height = 4.5, units = "in", res = 300)
 Mucro_Length <- ggplot(Mucros, aes(x = Mucro.linear.Length..µm. , fill = IDed.by)) +
   geom_histogram(position = "dodge", binwidth = 5) +
   labs(title = "Mucro Length", y = "Count", x = "Mucro Length (µm)") +
@@ -140,7 +154,9 @@ Mucro_Length <- ggplot(Mucros, aes(x = Mucro.linear.Length..µm. , fill = IDed.b
   geom_text(stat = "bin", binwidth = 5, aes(label = ifelse(..count.. > 0, round((..xmax.. + ..xmin..) / 2, 0), ""), y = ..count..), position = position_dodge(width = 0.9), vjust = -0.5)+ #label bars with x values (bin midpoint)
   scale_y_continuous(limits = c(0, 4.1))
 Mucro_Length
+#dev.off()
 
+#tiff(paste0(LakeName, "_Test_Mucro_Segments.tiff"), width = 10, height = 4.5, units = "in", res = 300)
 Mucro_Segments <- ggplot(Mucros, aes(x = Mucro.Sutures , fill = IDed.by)) +
   geom_histogram(position = "dodge", binwidth = 1) +
   labs(title = "Mucro Segments", y = "Count", x = "Mucro Segments (count)") +
@@ -155,7 +171,9 @@ Mucro_Segments <- ggplot(Mucros, aes(x = Mucro.Sutures , fill = IDed.by)) +
   geom_text(stat = "bin", binwidth = 1, aes(label = ifelse(..count.. > 0, round((..xmax.. + ..xmin..) / 2, 0), ""), y = ..count..), position = position_dodge(width = 0.9), vjust = -0.5) #label bars with x values (bin midpoint)
   #scale_y_continuous(limits = c(0, 4.1))
 Mucro_Segments
+#dev.off()
 
+#tiff(paste0(LakeName, "_Test_Carapace_Length.tiff"), width = 13, height = 6, units = "in", res = 300)
 Carapace_Length <- ggplot(Carapaces, aes(x = Carapace.Length..µm. , fill = IDed.by)) +
   geom_histogram(position = "dodge", binwidth = 10) +
   labs(title = "Carapace Length", y = "Count", x = "Carapace Length (µm)") +
@@ -170,6 +188,7 @@ Carapace_Length <- ggplot(Carapaces, aes(x = Carapace.Length..µm. , fill = IDed
   geom_text(stat = "bin", binwidth = 10, aes(label = ifelse(..count.. > 0, round((..xmax.. + ..xmin..) / 2, 0), ""), y = ..count..), position = position_dodge(width = 0.5), vjust = -0, angle = 60) #label bars with x values (bin midpoint)
   #scale_y_continuous(limits = c(0, 2.1))
 Carapace_Length
+#dev.off()
 
 
 #thoughts: I am unlikely to record anything at all if it is an unidentified fragment, while Evelyn records those often. Adele also less likely to record. No harm to data here. 
@@ -180,3 +199,8 @@ Carapace_Length
 
 #lengths look okay
 #mucro segments need work
+
+
+                   
+
+
