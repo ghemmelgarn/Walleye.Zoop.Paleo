0c43dd1bdf94d3d728f75080daac8c95309d7475
Grace Hemmelgarn <gracehemmelgarn@gmail.com>
2026-03-26 00:01:05 -0500

    Explores covariates initially chosen

--- Contemporary_Covariate_Exploration.R
+++ Contemporary_Covariate_Exploration.R
@@ -1,4 +1,5 @@
 #This script takes the contemporary model dataset and explores covariates to help us choose which ones to put in the model
+#Then it the second part it takes a deeper look at the chosen covariates
 
 
 library(tidyverse)
@@ -332,3 +333,178 @@ corrplot(selected.cor,
          tl.col = "black",
          tl.cex = 1)
 dev.off()
+
+
+#Part 2 ---------------------------------------------------------------------------------------------------------------------------
+
+#Import new dataset
+data2 <- read.csv("Data/Input/Contemporary_Dataset_2026_03_25.csv")
+
+#remove the fish and zoop data for now and literally everything that isn't a covariate
+data2.cov <- data2 %>% 
+  filter(!is.na(total_zoop_biomass)) %>%  #removes Hill south that does not have zoop data :(
+  select(lake_name:stock.yn, -lagoslakeid, -lagoslakeid_verm, -nhdhr_id)
+
+
+#which month range do I have more in situ secchi data?
+summary(data2.cov$secchi.meters.MPCA.Jun.to.Aug) #61 NAs
+summary(data2.cov$secchi.meters.MPCA.Jul.to.Sept) #51 NAs - use this one
+
+#isolate just chosen quantitative covariates and rename them shorter so you can read what they are in ggpairs
+#also calcualte the log of chla
+chosen.quant <- data2.cov %>% 
+  select(remote.chla, remote.CDOM, secchi.meters.MPCA.Jul.to.Sept, cond_uscm_exact_year, gdd.year.5c,
+         precip_10yr_avg_mm, area_ha, depth.max.m, photic_prop_secchi.meters.MPCA.Jul.to.Sept) %>% 
+  rename(secchi.m = secchi.meters.MPCA.Jul.to.Sept,
+         cond_uscm = cond_uscm_exact_year,
+         gdd.5C = gdd.year.5c,
+         precip_mm = precip_10yr_avg_mm,
+         photic_prop = photic_prop_secchi.meters.MPCA.Jul.to.Sept) %>% 
+  mutate(log.chla = log(remote.chla)) %>% 
+  relocate(log.chla, .after = remote.chla)
+
+#CHANGE NAME OF PRECIP VARIABLE TO 5
+
+#ggpairs
+pairs <- ggpairs(chosen.quant)
+#ggsave(filename = "Selected_Quantitative_Pairs.png", plot = pairs, width = 10, height = 10, units = "in", dpi = 300)
+
+
+
+#look at relationship between walleye yoy and zoops
+#isolate data
+yoy.zoop <- data2 %>% 
+  select(WAE.YOY.CPUE, total_zoop_biomass) %>% 
+  filter(!is.na(WAE.YOY.CPUE) & !is.na(total_zoop_biomass))
+
+#scatterplot
+yoy.zoop.plot <- ggplot(data = yoy.zoop, aes(x = WAE.YOY.CPUE, y = total_zoop_biomass))+
+  geom_point()+
+  theme_classic()
+#ggsave(filename = "Walleye_YOY_v_Total_Zoop_Biomass.png", plot = yoy.zoop.plot, width = 4, height = 4, units = "in", dpi = 300)
+
+
+cor(yoy.zoop$total_zoop_biomass, y =yoy.zoop$WAE.YOY.CPUE)
+#cor = 0.11
+#THESE ARE NOT CORRELATED
+
+#filter out only data with all the selected covariates to look at sample size
+sample <- data2.cov %>% 
+  filter(!is.na(remote.chla) & 
+           !is.na(remote.CDOM) & 
+           !is.na(secchi.meters.MPCA.Jul.to.Sept) & 
+           !is.na(cond_uscm_exact_year) &
+           !is.na(gdd.year.5c) & 
+           !is.na(precip_10yr_avg_mm) & 
+           !is.na(area_ha) & 
+           !is.na(depth.max.m) & 
+           !is.na(photic_prop_secchi.meters.MPCA.Jul.to.Sept) &
+           !is.na(lake_connectivity_class) &
+           !is.na(SpinyWaterflea.yn) &
+           !is.na(ZebraMussel.yn))
+unique(sample$lake_name)
+#This gets me 13 lake-years from 10 lakes... not good
+
+
+#again but don't require conductivity because maybe correlated to temp
+sample.no.cond <- data2.cov %>% 
+  filter(!is.na(remote.chla) & 
+           !is.na(remote.CDOM) & 
+           !is.na(secchi.meters.MPCA.Jul.to.Sept) & 
+           !is.na(gdd.year.5c) & 
+           !is.na(precip_10yr_avg_mm) & 
+           !is.na(area_ha) & 
+           !is.na(depth.max.m) & 
+           !is.na(photic_prop_secchi.meters.MPCA.Jul.to.Sept) &
+           !is.na(lake_connectivity_class) &
+           !is.na(SpinyWaterflea.yn) &
+           !is.na(ZebraMussel.yn))
+unique(sample.no.cond$lake_name)
+#Now we are at 58 from 24 lakes... better but still not amazing
+
+#how many NA's in each covariate in entire dataset
+summary(chosen.quant)
+#chla = 115
+#CDOM = 115
+#secchi/photic_prop = 51
+#conductivity = 118
+#everything else = 0
+
+#THIS SUGGESTS WE SHOULD USE SECCHI, NOT CHLA + CDOM
+#what would that give for a sample size?
+sample.no.cond.chla.cdom <- data2.cov %>% 
+  filter(!is.na(secchi.meters.MPCA.Jul.to.Sept) & 
+           !is.na(gdd.year.5c) & 
+           !is.na(precip_10yr_avg_mm) & 
+           !is.na(area_ha) & 
+           !is.na(depth.max.m) & 
+           !is.na(photic_prop_secchi.meters.MPCA.Jul.to.Sept) &
+           !is.na(lake_connectivity_class) &
+           !is.na(SpinyWaterflea.yn) &
+           !is.na(ZebraMussel.yn))
+unique(sample.no.cond.chla.cdom$lake_name)
+#this give us 153 lake-years from 35 lakes - much better...
+
+# Check for trout lakes
+trout <- data2 %>% 
+  filter(BKT.CPUE > 0 | LAT.CPUE > 0 | RBT.CPUE > 0)
+#15 lake-years from 5 lakes (all the years from these 5 lakes)
+
+
+# Check for high CDOM lakes
+max(data2$remote.CDOM, na.rm = TRUE)
+min(data2$remote.CDOM, na.rm = TRUE)
+#tait has high CDOM (5.5) - and internet says it is stained. Should I remove? - 3 lake-years
+#next highest are all VNP lakes: Rainy (4.5), Sand Point (3.9), Namakan (3.8)
+#Idea of removing them would be so that secchi is not low because of high CDOM - I really don't think these are stained enough to make that a problem??
+#What would be a good cutoff?
+
+
+# Do the PCA again
+#get rid of log chla
+pca.data <- chosen.quant %>% 
+  select(-log.chla)
+
+pca_results <- prcomp(na.omit(pca.data),
+                      center = TRUE, 
+                      scale. = TRUE)
+summary(pca_results)
+
+#biplots
+
+pc1v2 <- fviz_pca_biplot(pca_results, 
+                repel = TRUE,            # Avoids overlapping text
+                col.var = "red",         # Color for variables
+                col.ind = "steelblue")+  # Color for lakes
+  scale_y_continuous(limits = c(-4, 4)) +
+  scale_x_continuous(limits = c(-4, 4)) +
+  labs(title = "PC1 (0.50) v PC2 (0.22)")
+pc1v2
+#ggsave(filename = "Chosen_PC1vPC2.png", plot = pc1v2, width = 6, height = 6, units = "in", dpi = 300)
+#why are secchi and chla opposite each other?
+
+
+pc2v3 <- fviz_pca_biplot(pca_results, 
+                axes = c(2,3),
+                repel = TRUE,            # Avoids overlapping text
+                col.var = "red",         # Color for variables
+                col.ind = "steelblue")+  # Color for lakes
+  scale_y_continuous(limits = c(-4, 4)) +
+  scale_x_continuous(limits = c(-4, 4)) +
+  labs(title = "PC2 (0.22) v PC3 (0.13")
+pc2v3
+#ggsave(filename = "Chosen_PC2vPC3.png", plot = pc2v3, width = 6, height = 6, units = "in", dpi = 300)
+
+
+pc1v3 <- fviz_pca_biplot(pca_results, 
+                axes = c(1,3),
+                repel = TRUE,            # Avoids overlapping text
+                col.var = "red",         # Color for variables
+                col.ind = "steelblue")+  # Color for lakes
+  scale_y_continuous(limits = c(-4, 4)) +
+  scale_x_continuous(limits = c(-4, 4)) +
+  labs(title = "PC1 (0.50) v PC3 (0.13)")
+pc1v3
+#ggsave(filename = "Chosen_PC1vPC3.png", plot = pc1v3, width = 6, height = 6, units = "in", dpi = 300)
+
+
